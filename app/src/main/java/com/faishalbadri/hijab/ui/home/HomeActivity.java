e0a061a3bd08f0b88068734d7b364fafc50cbd84
Ahmad Faishal Albadri <faishalalbadri1@gmail.com>
2017-10-30 13:14:43 +0700

    home

---
+++ app/src/main/java/com/faishalbadri/hijab/ui/home/HomeActivity.java
@@ -0,0 +1,14 @@
+package com.faishalbadri.hijab.ui.home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.faishalbadri.hijab.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_home);
+  }
+}
