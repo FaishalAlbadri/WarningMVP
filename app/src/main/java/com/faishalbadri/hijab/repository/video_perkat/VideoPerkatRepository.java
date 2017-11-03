5fcffca8380a83ef3b4e14f27dd6c950be8a630a
Muhammad Fikri Imaduddin Masyhuri <fikri.im@gmail.com>
2017-11-03 23:26:39 +0700

    cleaning

---
+++ app/src/main/java/com/faishalbadri/hijab/repository/video_perkat/VideoPerkatRepository.java
@@ -0,0 +1,14 @@
+package com.faishalbadri.hijab.repository.video_perkat;
+
+import com.faishalbadri.hijab.repository.video_perkat.remote.VideoPerkatRemote;
+
+/**
+ * Created by fikriimaduddin on 11/3/17.
+ */
+
+public class VideoPerkatRepository {
+
+  public VideoPerkatRepository(VideoPerkatRemote videoPerkatRemote) {
+
+  }
+}
---
+++ app/src/main/java/com/faishalbadri/hijab/ui/video_perkat/VideoPerkatActivity.java
@@ -0,0 +1,14 @@
+package com.faishalbadri.hijab.ui.video_perkat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.faishalbadri.hijab.R;
+
+public class VideoPerkatActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_video_perkat);
+  }
+}
