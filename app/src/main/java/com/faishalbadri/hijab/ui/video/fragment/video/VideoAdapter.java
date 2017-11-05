173bdadef9fbf416867075714bde67c35ce785fd
Muhammad Fikri Imaduddin Masyhuri <fikri.im@gmail.com>
2017-11-05 11:50:48 +0700

    Merge branch 'master' of https://github.com/FaishalAlbadri/WarningMVP
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- app/src/main/java/com/faishalbadri/hijab/ui/video/fragment/video/VideoAdapter.java
+++ app/src/main/java/com/faishalbadri/hijab/ui/video/fragment/video/VideoAdapter.java
@@ -11,9 +11,11 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.Adapter;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.bumptech.glide.Glide;
@@ -24,7 +26,13 @@ import com.faishalbadri.hijab.data.PojoVideo.VideoBean;
 import com.faishalbadri.hijab.ui.detail_activity.video.DetailVideoActivity;
 import com.faishalbadri.hijab.ui.video.fragment.video.VideoAdapter.ViewHolder;
 import com.faishalbadri.hijab.util.Server;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubeStandalonePlayer;
+import com.google.android.youtube.player.YouTubeThumbnailLoader;
+import com.google.android.youtube.player.YouTubeThumbnailLoader.ErrorReason;
+import com.google.android.youtube.player.YouTubeThumbnailLoader.OnThumbnailLoadedListener;
 import com.google.android.youtube.player.YouTubeThumbnailView;
+import com.google.android.youtube.player.YouTubeThumbnailView.OnInitializedListener;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -46,13 +54,52 @@ public class VideoAdapter extends Adapter<ViewHolder> {
   public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
     View view = LayoutInflater.from(context).inflate(R.layout.item_video_full, parent, false);
     final ViewHolder viewHolder = new ViewHolder(view);
+
     return viewHolder;
   }
 
   @Override
   public void onBindViewHolder(ViewHolder holder, int position) {
     final VideoBean listitem = list_video.get(position);
-
+    RequestOptions options = new RequestOptions().fitCenter().format(DecodeFormat.PREFER_ARGB_8888)
+        .override(500, 500);
+    Glide.with(context)
+        .load(Server.BASE_IMG_YT + listitem.getVideo() + Server.IMG_YT_FORMAT)
+        .apply(options)
+        .into(holder.thumbnailViewVideoItem);
+//    final YouTubeThumbnailLoader.OnThumbnailLoadedListener onThumbnailLoadedListener = new OnThumbnailLoadedListener() {
+//      @Override
+//      public void onThumbnailLoaded(YouTubeThumbnailView youTubeThumbnailView, String s) {
+//        youTubeThumbnailView.setVisibility(View.VISIBLE);
+//      }
+//
+//      @Override
+//      public void onThumbnailError(YouTubeThumbnailView youTubeThumbnailView,
+//          ErrorReason errorReason) {
+//
+//      }
+//    };
+//    holder.thumbnailViewVideoItem.initialize(Server.YT_CODE, new OnInitializedListener() {
+//      @Override
+//      public void onInitializationSuccess(YouTubeThumbnailView youTubeThumbnailView, YouTubeThumbnailLoader youTubeThumbnailLoader) {
+//        youTubeThumbnailLoader.setVideo(listitem.getVideo());
+//        youTubeThumbnailLoader.setOnThumbnailLoadedListener(onThumbnailLoadedListener);
+//      }
+//
+//      @Override
+//      public void onInitializationFailure(YouTubeThumbnailView youTubeThumbnailView, YouTubeInitializationResult youTubeInitializationResult) {
+//        Toast.makeText(context, "Check your internet", Toast.LENGTH_SHORT).show();
+//      }
+//    });
+    holder.txtTitleItemVideo.setText(listitem.getJudul_video());
+    holder.txtDurationItemVideo.setText(listitem.getDuration());
+    holder.thumbnailViewVideoItem.setOnClickListener(new OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        Intent intent = YouTubeStandalonePlayer.createVideoIntent((Activity) context, Server.YT_CODE, listitem.getVideo(), 3000, true, false);
+        context.startActivity(intent);
+      }
+    });
   }
 
   @Override
@@ -74,6 +121,8 @@ public class VideoAdapter extends Adapter<ViewHolder> {
       ButterKnife.bind(this, itemView);
     }
   }
+
+
 
   public Drawable getSelectedItemDrawable() {
     int[] attrs = new int[]{R.attr.selectableItemBackground};
