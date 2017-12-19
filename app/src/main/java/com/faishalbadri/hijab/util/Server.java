771d294d4aebe319a2a363527de32642de901216
Ahmad Faishal Albadri <faishalalbadri1@gmail.com>
2017-12-19 23:45:54 +0700

    add new migration-server

--- app/src/main/java/com/faishalbadri/hijab/util/Server.java
+++ app/src/main/java/com/faishalbadri/hijab/util/Server.java
@@ -8,11 +8,19 @@ import com.rengwuxian.materialedittext.MaterialEditText;
 
 public class Server {
 
+
+  public final static String BASE_URL_REVAMP = "http://api.santriprogrammer.com/pinkyhijab/api/v1/";
+  public final static String BASE_API = "http://api.santriprogrammer.com/pinkyhijab/api/";
+
+
   public final static String BASE_URL = "http://api.santriprogrammer.com/hijab/library/";
   public final static String BASE_IMG = "http://api.santriprogrammer.com/hijab/img/";
   public static String BASE_IMG_YT = "https://i.ytimg.com/vi/";
   public static String IMG_YT_FORMAT = "/mqdefault.jpg";
   public static String YT_CODE = "AIzaSyAZtqvIpixBElGAIewxDbfOABF0V35TWTY";
+
+  public static final String EMAIL = "instagramofficialcourse@gmail.com";
+  public static final String PASSWORD = "instaindo";
 
   public static boolean isEmpty(MaterialEditText materialEditText) {
         /*jika banyak huruf lebih dari 0*/
