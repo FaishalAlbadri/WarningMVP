95cedd627076053a02cc87fce80302775bd16879
Ahmad Faishal Albadri <faishalalbadri1@gmail.com>
2017-12-20 00:05:33 +0700

    Baca ini sebelum mengerjakan

---
+++ app/src/main/java/com/faishalbadri/hijab/BacaSebelumMengerjakan.java
@@ -0,0 +1,42 @@
+package com.faishalbadri.hijab;
+
+/**
+ * Created by faishal on 19/12/17.
+ */
+
+public class BacaSebelumMengerjakan {
+
+  /**
+   klo udah selesai dan finish Migration-server nya tulis sini.. per-API tulisnya
+
+   klo mau migrasi yang dikerjain pertama pojo -> repo -> XxxContract -> XxxPresenter ->
+   XxxAdapter -> XxxFragment/XxxActivity.
+
+   pindahin folder yang mau di migrasi kedalam revamp WAJIB
+   */
+
+  /**
+   Migration-server yang udah gw kerjain (Faishal)
+   1. categories
+   2. city_event
+   3. ebook_categories
+   4. register
+   5. sponsor
+   6. event
+   7. event_city
+   8. search_event
+   */
+
+  /**
+   Migration-server yang udah gw kerjain (Fikri)
+   1.
+   2.
+   3.
+   4.
+   5.
+   6.
+   7.
+   8.
+   */
+
+}
