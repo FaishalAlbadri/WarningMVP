58fea80b0a1efe795814f6a830f7ae7a98c27111
Ahmad Faishal Albadri <faishalalbadri1@gmail.com>
2017-12-23 02:40:00 +0700

    Planning!!!

--- app/src/main/java/com/faishalbadri/hijab/BacaSebelumMengerjakan.java
+++ app/src/main/java/com/faishalbadri/hijab/BacaSebelumMengerjakan.java
@@ -10,5 +10,20 @@ public class BacaSebelumMengerjakan {
      Sisa yang belum :
      1. verify_code
      2. news_slider
+
+     Planning :
+     1. cleancode variable, package, anything name in this project (fikri)
+     2. revamp ui & ux.. if needed
+     2. condition layout when no internet access or data null
+     3. change volley to retrofit (faishal)
+     4. forget password(faishal)
+     5. must using paggination on recyclerview (faishal & fikri)
+     6. users can edit data profile(fikri)
+     7. new feature "Community"
+
+
+     Big planning :
+     1. migration to reactive-programming using rxjava2 and dagger2 (target : faishal & fikri,
+     maybe alvin)
    */
 }
