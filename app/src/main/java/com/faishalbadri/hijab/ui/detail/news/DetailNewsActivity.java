26db65cd25a8bcd667e4e0fdea2c9ab741f54037
Muhammad Fikri Imaduddin Masyhuri <fikri.im@gmail.com>
2018-01-02 21:12:54 +0700

    Merge branch 'develop' of https://github.com/FaishalAlbadri/WarningMVP into develop
    # Conflicts:
    #	app/src/main/java/com/faishalbadri/hijab/ui/detail/news/DetailNewsActivity.java
    #	app/src/main/java/com/faishalbadri/hijab/ui/news/fragment/category/NewsCategoryFragment.java

--- app/src/main/java/com/faishalbadri/hijab/ui/detail/news/DetailNewsActivity.java
+++ app/src/main/java/com/faishalbadri/hijab/ui/detail/news/DetailNewsActivity.java
@@ -54,7 +54,7 @@ public class DetailNewsActivity extends AppCompatActivity implements DetailNewsV
   ScrollView scrollviewDetailNews;
   @BindView(R.id.layout_no_internet_acces)
   RelativeLayout layoutNoInternetAcces;
-  private String title, image, desc, id_isi;
+  private String title, image, desc, id_content;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -145,6 +145,6 @@ public class DetailNewsActivity extends AppCompatActivity implements DetailNewsV
 
   @OnClick(R.id.layout_no_internet_acces)
   public void onViewClicked() {
-    detailNewsPresenter.getData(id_isi);
+    detailNewsPresenter.getData(id_content);
   }
 }
