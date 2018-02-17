3711b3d525bfe7c7901bf2ac206702ce8821ce8a
Ahmad Faishal Albadri <faishalalbadri1@gmail.com>
2018-02-18 00:43:28 +0700

    edit string

--- app/src/main/java/com/faishalbadri/hijab/ui/intro/IntroActivity.java
+++ app/src/main/java/com/faishalbadri/hijab/ui/intro/IntroActivity.java
@@ -43,7 +43,7 @@ public class IntroActivity extends AppCompatActivity {
       // changing the next button text 'NEXT' / 'GOT IT'
       if (position == layouts.length - 1) {
         // last page. make button text to GOT IT
-        btnNext.setText(getString(R.string.text_register));
+        btnNext.setText(getString(R.string.text_login));
         btnSkip.setVisibility(View.GONE);
       } else {
         // still pages are left
